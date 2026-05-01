d02586bdb72bb51845c3156c2ecbbd59a6056343
Rati Montreewat <ratixoxo@gmail.com>
2026-05-01 18:15:41 +0700

    Refactor (./web): Relocating analytics module

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -9,7 +9,7 @@
 	import '../app.postcss';
 	import { page } from '$app/state';
 	import CookieConsentBanner from '$lib/ui/cookie/CookieConsentBanner.svelte';
-	import GoogleAnalytics from '$lib/analytics/GoogleAnalytics.svelte';
+	import GoogleAnalytics from '$lib/product-analytics/GoogleAnalytics.svelte';
 	import { THEME_STORAGE_KEY } from '$lib/ui/daisyui/ThemeSwitcher.svelte';
 
 	type Props = {
